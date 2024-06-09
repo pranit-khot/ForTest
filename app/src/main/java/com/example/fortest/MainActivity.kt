2f6a70fcd2f04b91a934e7b30c9ccaac5398d466
Pranit Khot <khotpranit3@gmail.com>
2024-06-09 19:33:36 +0530

    test new jenkins

--- app/src/main/java/com/example/fortest/MainActivity.kt
+++ app/src/main/java/com/example/fortest/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Jenkins $name!",
+        text = "new $name!",
         modifier = modifier
     )
 }
