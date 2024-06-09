786c1e13a9587c6ffe983fa55fd379bbaee30675
Pranit Khot <khotpranit3@gmail.com>
2024-06-09 19:04:15 +0530

    test new commit

--- app/src/main/java/com/example/fortest/MainActivity.kt
+++ app/src/main/java/com/example/fortest/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Test $name!",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
